9d8cde9d2bea01ca289af21a491b14de997988e6
oqbahdx <oqbahdx@gamil.com>
2026-05-31 18:50:33 +0300

    add SplashPage with animations and loading dots

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'core/theme/app_colors.dart';
 import 'features/auth/presentation/bloc/register/register_bloc.dart';
-import 'features/auth/presentation/pages/register_page.dart';
+import 'features/splash/presentation/pages/splash_page.dart';
 import 'injection.dart';
 
 void main() {
@@ -15,16 +16,23 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: AppColors.primary,
-      ),
-      home: BlocProvider(
-        create: (context) => getIt<RegisterBloc>(),
-        child: const RegisterPage(),
-      ),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            useMaterial3: true,
+            colorSchemeSeed: AppColors.primary,
+          ),
+          home: BlocProvider(
+            create: (context) => getIt<RegisterBloc>(),
+            child: const SplashPage(),
+          ),
+        );
+      },
     );
   }
 }
